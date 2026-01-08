0f6e4e291ef3b74bf0ed00de8a1166ec159a8f41
Konstantin <bodrov.2005@inbox.ru>
2026-01-08 19:07:30 +0500

    .

--- server.py
+++ server.py
@@ -5,6 +5,6 @@ PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Сервер запущен на http://localhost:{PORT}")
+    print(f"Сервер запущен на http://localhost:{PORT}/pages/")
     print("Для остановки сервера нажмите Ctrl+C")
     httpd.serve_forever()
