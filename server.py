7d46f4fcd07dee6b44e3b81d9a97c230c642621c
Konstantin <bodrov.2005@inbox.ru>
2026-01-08 17:31:47 +0500

    .

--- server.py
+++ server.py
@@ -1,13 +1,5 @@
-#!/usr/bin/env python3
-"""
-Простой HTTP сервер для проекта "Читака"
-"""
 import http.server
 import socketserver
-import os
-
-# Убедимся, что мы в правильной директории
-os.chdir('/workspace/Читака')
 
 PORT = 8000
 Handler = http.server.SimpleHTTPRequestHandler
